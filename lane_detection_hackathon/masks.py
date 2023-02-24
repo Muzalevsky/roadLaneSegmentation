eed28b7ca08a6dfa1b32b60c770f621d341b68e5
Lenke <seryhelena@gmail.com>
2023-02-24 20:28:13 +0300

    [develop] [RF]: color map - initial

---
+++ lane_detection_hackathon/masks.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class MaskColorMap(Enum):
+    SYD = (60, 15, 67)  # solid yellow dividing
+    BWG = (142, 35, 8)  # broken white guiding
+    SWD = (180, 173, 43)  # solid whilte dividing
+    SWS = (0, 0, 192)  # solid white stopping
+    CWYZ = (153, 102, 153)  # zebra
