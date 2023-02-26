64dfa854ae7af39a0ca2d0e8a98ef083c9e51a3b
Lenke <seryhelena@gmail.com>
2023-02-26 22:39:32 +0300

    [develop] [NF]: add fs functions

--- lane_detection_hackathon/utils/fs.py
+++ lane_detection_hackathon/utils/fs.py
@@ -1,15 +1,40 @@
+import json
+from functools import lru_cache
+
 import cv2
 import numpy as np
+import yaml
 
 
-def read_image(fpath: str) -> np.ndarray:
-    img = cv2.imread(fpath)
-    if img is not None:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+@lru_cache(maxsize=128)
+def read_image(fpath: str, gray_scale: bool = False) -> np.ndarray:
+    flag = None
+    if gray_scale:
+        flag = cv2.IMREAD_GRAYSCALE
+
+    img = cv2.imread(fpath, flags=flag)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
     return img
 
 
-def read_mask(fpath: str) -> np.ndarray:
-    """Read grayscale index-based mask."""
-    mask = cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)
-    return mask
+def read_yaml(fpath: str) -> dict:
+    with open(fpath) as fp:
+        data = yaml.safe_load(fp)
+    return data
+
+
+def save_yaml(fpath: str, data: dict):
+    with open(fpath, "w") as fp:
+        yaml.safe_dump(data, fp)
+
+
+def read_json(fpath: str) -> dict:
+    with open(fpath) as fp:
+        data = json.load(fp)
+    return data[0]
+
+
+def save_json(fpath: str, data: dict):
+    with open(fpath, "w") as fp:
+        json.dump(data, fp, indent=4, ensure_ascii=False)
