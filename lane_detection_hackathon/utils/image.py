43a28bf98a3c7a0120be49343acc403d9997b128
Muzalevsky Alexander <muzal2010@yandex.ru>
2023-03-05 20:00:19 +0300

    [develop] Add image+mask=overlay notebook and method

--- lane_detection_hackathon/utils/image.py
+++ lane_detection_hackathon/utils/image.py
@@ -3,3 +3,7 @@ import numpy as np
 
 def is_gray(img: np.array) -> bool:
     return len(img.shape) == 2 or (len(img.shape) == 3 and img.shape[2] == 1)
+
+
+def overlay(img: np.array, mask: np.array) -> np.array:
+    return np.where(mask != (0, 0, 0), mask, img)
